407cab33117e810ddfb9b1d191b8416c0fb7359b
Lil Zhang <lilzhang90s@163.com>
2016-02-18 01:03:38 +0800

    add freemarker support

---
+++ src/main/java/controller/FTLController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by LilZhang on 2016/2/18.
+ */
+@Controller
+public class FTLController {
+    private static final Log logger = LogFactory.getLog(FTLController.class);
+
+    @RequestMapping(value = "/show")
+    public ModelAndView showFTL() {
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("title", "FTL title");
+        mv.addObject("content", "FTL content");
+        return mv;
+    }
+}
